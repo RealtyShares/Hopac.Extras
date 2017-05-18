1ee199a88376929b68b4eaa955bccf6bfbfd989d
Tracy Gonzalez <tracy@realtyshares.com>
2017-05-18 13:46:21 -0700

    creating nugetsource branch

--- src/Hopac.Extras/Scripts/load-references.fsx
+++
@@ -1,8 +0,0 @@
-// Warning: generated file; your changes could be lost when a new file is generated.
-#I __SOURCE_DIRECTORY__
-#r @"System.dll"
-#r @"System.Core.dll"
-#r @"System.Numerics.dll"
-#r @"..\..\..\packages\Hopac\lib\net45\Hopac.Core.dll"
-#r @"..\..\..\packages\Hopac\lib\net45\Hopac.dll"
-#r @"..\..\..\packages\Hopac\lib\net45\Hopac.Platform.dll"
--- tests/Hopac.Extras.Tests/Utils.fs
+++
@@ -1,8 +0,0 @@
-﻿[<AutoOpen>]
-module Hopac.Extras.Tests.Utils
-
-open Hopac.Infixes
-open Hopac
-
-let runWith defaultOp testedOp = testedOp <|> defaultOp |> run
-
